8e11f68e13f2be0fd678e02627ec287658168512
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2017-04-30 07:31:23 +0900

    #2 Formから送信された値を表示してみよう。

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  "Hello world"
+  erb :index
+end
+
+get '/contact_new' do
+  erb :form
+end
+
+post '/contacts' do
+  @name = params[:name]
+  erb :result
 end
